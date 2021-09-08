46151fe71d7005e48c6febfdf56c230000caf58c
wangweijun123 <262448350@qq.com>
2021-09-08 15:05:23 +0800

    Update Circle.h

--- cpp/Circle.h
+++ cpp/Circle.h
@@ -1,1 +1,19 @@
+#include "Point.h"
 
+const double PI = 3.14;
+
+class Circle {
+
+public:
+	int m_r;
+
+	Point center;
+
+	void setR(int r);
+
+	void setCenter(Point p);
+	double calculateZC();
+
+	void container(Point outPoint);
+
+};
