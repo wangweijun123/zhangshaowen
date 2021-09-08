5b10f4a5eb38cb345a4551c9c4e18d82b2af4759
wangweijun123 <262448350@qq.com>
2021-09-08 15:06:04 +0800

    Create Point.cpp

---
+++ cpp/Point.cpp
@@ -0,0 +1,24 @@
+#include "Point.h"
+
+int Point::getX() {
+	return x;
+}
+
+void  Point::setX(int x) {
+	this->x = x;
+}
+
+int  Point::getY() {
+	return y;
+}
+
+void  Point::setY(int y) {
+	this->y = y;
+}
+
+Point::Point() {
+
+}
+Point::~Point() {
+
+}
